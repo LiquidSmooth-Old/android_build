07b99059164946c696632524b81f610fc1400b19
jdkoreclipse <jdkoreclipse@gmail.com >
2012-08-03 00:41:45 -0300

    Pass O3 and Ofast in TARGET_linux-arm.mk

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -70,7 +70,7 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=    -O2 \
+TARGET_arm_CFLAGS :=    -O3 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops
@@ -81,7 +81,7 @@ TARGET_arm_CFLAGS :=    -O2 \
 # compiled as ARM.
 ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
 TARGET_thumb_CFLAGS :=  -mthumb \
-                        -Os \
+                        -Ofast \
                         -fomit-frame-pointer \
                         -fno-strict-aliasing
 else
