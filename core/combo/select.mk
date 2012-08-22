c12c9cc33410e54b1c35ddc5eb61b0a0875f84df
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-08-22 16:17:04 +0200

    build: Don't add -fno-strict-aliasing to HOST compiler flags
    Change-Id: I07eef86f11ec3761e0922855cb6ff881c2773966
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -47,7 +47,10 @@ $(combo_target)HAVE_STRLCAT := 0
 $(combo_target)HAVE_KERNEL_MODULES := 0
 
 $(combo_target)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
-$(combo_target)RELEASE_CFLAGS := -O3 -g -Wstrict-aliasing=2 -Werror=strict-aliasing
+$(combo_target)RELEASE_CFLAGS := -O3 -g -Wstrict-aliasing=2
+ifneq ($(combo_target),HOST_)
+$(combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
+endif
 $(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
