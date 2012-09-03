b7a8b4d70e934c368b4c6e4496d46902f8eed3e3
LiquidSmooth <liquid0624@gmail.com>
2012-09-03 14:10:50 -0400

    Revert "Revert "Revert "Implement Custom Linaro Build Optimizations"""
    This reverts commit 7d305a79433fc32894092f221f94884e8ffa2ce0.

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -162,7 +162,7 @@ else
 TARGET_GLOBAL_CFLAGS +=	-mno-thumb-interwork
 endif
 
-TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden $(call cc-option,-std=gnu++11)
+TARGET_GLOBAL_CPPFLAGS += -fvisibility-inlines-hidden
 
 # More flags/options can be added here
 TARGET_RELEASE_CFLAGS := \
