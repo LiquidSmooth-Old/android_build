e8c5a6d5c209dfa3c3fce4dda744c18c3d8c3564
Jerome Pilliet <pilliet@univ-mlv.fr>
2012-12-17 11:12:50 +0100

    Some changes added to compile and run with Java 6 and Java 7.
      - correction of errors with Javac 6:
        - target 1.5 and source empty.
      - correction of errors with Javadoc 7:
        - strange behavior when duplicate classes are given.
    
    Change-Id: Ib814ede143f7799ef18e0fb1b733547519b8b0e5
    
    Conflicts:
    	core/main.mk

--- core/main.mk
+++ core/main.mk
@@ -120,6 +120,40 @@ $(warning ************************************************************)
 $(error Directory names containing spaces not supported)
 endif
 
+# Check for the correct version of java
+java_version := $(shell java -version 2>&1 | head -n 1 | grep '^java .*[ "]1\.[67][\. "$$]')
+ifneq ($(shell java -version 2>&1 | grep -i openjdk),)
+java_version :=
+endif
+ifeq ($(strip $(java_version)),)
+$(info ************************************************************)
+$(info You are attempting to build with an unsupported version)
+$(info of java.)
+$(info $(space))
+$(info Your version is: $(shell java -version 2>&1 | head -n 1).)
+$(info The correct version is: Java SE 1.6 or 1.7.)
+$(info $(space))
+$(info Please follow the machine setup instructions at)
+$(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
+$(info ************************************************************)
+endif
+
+# Check for the correct version of javac
+javac_version := $(shell javac -version 2>&1 | head -n 1 | grep '[ "]1\.[67][\. "$$]')
+ifeq ($(strip $(javac_version)),)
+$(info ************************************************************)
+$(info You are attempting to build with the incorrect version)
+$(info of javac.)
+$(info $(space))
+$(info Your version is: $(shell javac -version 2>&1 | head -n 1).)
+$(info The correct version is: 1.6 or 1.7.)
+$(info $(space))
+$(info Please follow the machine setup instructions at)
+$(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
+$(info ************************************************************)
+$(error stop)
+endif
+
 ifeq (darwin,$(HOST_OS))
 GCC_REALPATH = $(realpath $(shell which gcc))
 ifneq ($(findstring llvm-gcc,$(GCC_REALPATH)),)
