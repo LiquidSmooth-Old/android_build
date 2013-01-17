0746ff62d6deaf4ff7271cc085ea19c2ea0559a5
nocoast <voteforpedro8@gmail.com>
2013-01-17 15:32:23 -0600

    Added necessary 'endif' to my 4.6 override function
    Should work now ^_^

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -54,6 +54,7 @@ $(combo_target)GLOBAL_CFLAGS := O3 -g -Wstrict-aliasing=2
 ifneq ($(combo_target),HOST_)
 (combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
 endif
+endif
 $(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
