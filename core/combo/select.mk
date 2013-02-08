766e96292bb930c31427311090ab2cae3c345b9d
DAGr8 <dagr82003@yahoo.com>
2013-02-08 16:05:49 -0500

    use butter

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -46,14 +46,12 @@ $(combo_target)HAVE_STRLCPY := 0
 $(combo_target)HAVE_STRLCAT := 0
 $(combo_target)HAVE_KERNEL_MODULES := 0
 
-ifneq ($(TARGET_USE_02),true)
 $(combo_target)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
-$(combo_target)RELEASE_CFLAGS := -O2 -g -fno-strict-aliasing
-else
 $(combo_target)GLOBAL_CFLAGS := O3 -g -Wstrict-aliasing=2
 ifneq ($(combo_target),HOST_)
 (combo_target)RELEASE_CFLAGS += -Werror=strict-aliasing
-endif
+else
+$(combo_target)RELEASE_CFLAGS += -Wno-error=strict-aliasing
 endif
 $(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
 $(combo_target)GLOBAL_ARFLAGS := crsP
