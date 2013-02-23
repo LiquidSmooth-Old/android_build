62dfc1f9c65c0254be1cf1ec8871c846d31e521b
Justin Rogers <kejar31@Ubuntu-Laptop.(none)>
2013-02-23 15:26:28 -0600

    Add standard method for enabling mfpu and mcpu code optimization diffmerged from CM

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -1,17 +1,8 @@
 # Configuration for Linux on ARM.
 # Generating binaries for the ARMv7-a architecture and higher with NEON
 #
-ARCH_ARM_HAVE_THUMB_SUPPORT     := true
-ARCH_ARM_HAVE_FAST_INTERWORKING := true
-ARCH_ARM_HAVE_64BIT_DATA        := true
-ARCH_ARM_HAVE_HALFWORD_MULTIPLY := true
-ARCH_ARM_HAVE_CLZ               := true
-ARCH_ARM_HAVE_FFS               := true
-ARCH_ARM_HAVE_ARMV7A            := true
-ARCH_ARM_HAVE_TLS_REGISTER      := true
-ARCH_ARM_HAVE_VFP               := true
-ARCH_ARM_HAVE_VFP_D32           := true
-ARCH_ARM_HAVE_NEON              := true
+TARGET_ARCH_VARIANT_FPU := neon
+include $(BUILD_COMBOS)/arch/$(TARGET_ARCH)/armv7-a.mk
 
 ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a15)
 ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
@@ -28,14 +19,3 @@ ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS    := true
 ARCH_ARM_NEON_MEMSET_DIVIDER           := 132
 ARCH_ARM_NEON_MEMCPY_ALIGNMENT_DIVIDER := 224
 endif
-
-# Note: Hard coding the 'tune' value here is probably not ideal,
-# and a better solution should be found in the future.
-#
-arch_variant_cflags := \
-    -march=armv7-a \
-    -mfloat-abi=softfp \
-    -mfpu=neon
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
