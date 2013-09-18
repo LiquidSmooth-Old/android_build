4b0bb2f822995c168eced7ca4b19e820b8e3d107
harigollamudi <harigopal.gollamudi@linaro.org>
2013-09-18 15:06:54 +0100

    Changes done for compile and link NEON jpeg libs, remove the module support for kernel build
    Change-Id: Ic809a66884fda4d3c8552f838b97bb57ee263df8

--- target/board/generic/BoardConfig.mk
+++ target/board/generic/BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_ARCH := arm
 # that are slower to emulate. On the other hand, it is possible to emulate
 # application code generated with the NDK that uses NEON in the emulator.
 #
-TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
@@ -44,6 +44,7 @@ BUILD_EMULATOR_OPENGL_DRIVER := true
 
 # Build and enable the OpenGL ES View renderer. When running on the emulator,
 # the GLES renderer disables itself if host GL acceleration isn't available.
+
 USE_OPENGL_RENDERER := true
 
 # Set the phase offset of the system's vsync event relative to the hardware
@@ -75,3 +76,4 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 512
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+BUILD_KERNEL_MODULES := false
