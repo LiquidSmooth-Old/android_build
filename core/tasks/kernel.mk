e50220ea86f1b402ee6c69a54edcaf04d74a978f
liquid0624 <rjbirnesser@gmail.com>
2013-11-30 19:07:45 -0500

    kernel: allow to specify a custom toolchain for kernel build
    TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
    
    Also removed workaround for darwin (I6321fb7f6814b207c821d974766d945351b3f546)
    since the binaries got merged
    
    Change-Id: I12f3587ce7c85ee6d3741f0619a376b13a936a90
    
    Conflicts:
    	core/tasks/kernel.mk

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -108,8 +108,12 @@ ifeq ($(TARGET_ARCH),arm)
       # Check that the executable is here.
       ccache := $(strip $(wildcard $(ccache)))
     endif
-    ifeq ($(HOST_OS),darwin)
-      ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-4.6/bin/arm-eabi-"
+    ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
+      ifeq ($(HOST_OS),darwin)
+        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
+      else
+        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
+      endif
     else
       ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
     endif
