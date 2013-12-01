9e9ba75ed7bab46a0f3ba86556b1f9f69c7ee9fe
liquid0624 <rjbirnesser@gmail.com>
2013-11-30 22:56:43 -0500

    This stuff is included in liquid vendor

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -22,12 +22,5 @@
 PRODUCT_PACKAGES := \
     VoiceDialer
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
-
-PRODUCT_COPY_FILES := \
-    device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
