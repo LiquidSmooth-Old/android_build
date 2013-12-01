596d2721a1f4f1c83f7145cce02b8ee75b3b2606
liquid0624 <rjbirnesser@gmail.com>
2013-11-30 22:43:04 -0500

    Support for devices that use liquid makefiles

--- core/product_config.mk
+++ core/product_config.mk
@@ -184,11 +184,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+  ifneq ($(LIQUID_BUILD),)
+    all_product_configs := $(shell ls device/*/$(LIQUID_BUILD)/liquid.mk)
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif # LIQUID_BUILD
 endif
 
+ifeq ($(LIQUID_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -210,6 +215,10 @@ $(foreach f, $(all_product_configs),\
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
