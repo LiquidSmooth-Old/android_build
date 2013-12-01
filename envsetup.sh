61869c28395f7310bd013e852d96daf4a6b45a05
liquid0624 <rjbirnesser@gmail.com>
2013-12-01 01:47:28 -0500

    Remove unused default lunch menu items

--- envsetup.sh
+++ envsetup.sh
@@ -445,10 +445,6 @@ function add_lunch_combo()
 }
 
 # add the default one here
-add_lunch_combo aosp_arm-eng
-add_lunch_combo aosp_x86-eng
-add_lunch_combo aosp_mips-eng
-add_lunch_combo vbox_x86-eng
 
 function print_lunch_menu()
 {
