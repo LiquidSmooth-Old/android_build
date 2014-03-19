83a8d69f8ef045cf12a65026ec06650997a93ffe
CPA-Poke <brock.shahan@cox.net>
2014-03-19 00:10:27 -0500

    Kill the goldfish emulator crap with fire.
    This reverts the following commits that break building on older
    devices:
    
    Revert "Android Emulator [ARM]"
    
    This reverts commit fcb6bfc501db9538ffec90e8729e274e09717699.
    
    Revert "add camera.goldfish.jpeg to emulator targets and mini targets"
    
    This reverts commit d9b0e80c1d2a70dae1ce7905ff4bfd554029bb88.
    
    Revert "pull goldfish specific stuffs from emulator.mk"
    
    This reverts commit c3eb68c4ba383e7de2dd9b761d96dbe646586845.

--- target/product/emulator.mk
+++ target/product/emulator.mk
@@ -39,7 +39,6 @@ PRODUCT_PACKAGES += \
 # Device modules
 PRODUCT_PACKAGES += \
     egl.cfg \
-    gralloc.goldfish \
     libGLESv1_CM_emulation \
     lib_renderControl_enc \
     libEGL_emulation \
@@ -48,16 +47,4 @@ PRODUCT_PACKAGES += \
     libGLESv2_emulation \
     libGLESv1_enc \
     qemu-props \
-    qemud \
-    camera.goldfish \
-    camera.goldfish.jpeg \
-    lights.goldfish \
-    gps.goldfish \
-    sensors.goldfish
-
-
-PRODUCT_COPY_FILES += \
-    device/generic/goldfish/fstab.goldfish:root/fstab.goldfish \
-    device/generic/goldfish/init.goldfish.rc:root/init.goldfish.rc \
-    device/generic/goldfish/init.goldfish.sh:system/etc/init.goldfish.sh \
-    device/generic/goldfish/ueventd.goldfish.rc:root/ueventd.goldfish.rc
+    qemud
