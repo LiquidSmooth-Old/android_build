a98f3d446178f94e5b1aabca50d5ca3b933a9053
liquid0624 <rjbirnesser@gmail.com>
2014-05-22 01:17:56 -0400

    Redo locales and include in all builds

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -33,10 +33,10 @@ PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 #$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
-#$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
+$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
 # Get a list of languages.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
 
 # Get everything else from the parent package
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
