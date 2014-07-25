e27a833aee7c24f14ca4f29e5da2dbc3880faf58
Chet Kener <Cl3Kener@gmail.com>
2014-07-25 08:51:56 -0400

    Add Linker Optimizations
    * Currently there are zero linker optimizations. This should spice things up
    
    Change-Id: I9ba468e6f5f6ae605fbae792d49806476920fae7
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -48,7 +48,7 @@ $(combo_target)HAVE_KERNEL_MODULES := 0
 
 $(combo_target)GLOBAL_CFLAGS := -fstrict-aliasing -Wstrict-aliasing=3 -Werror=strict-aliasing -fno-exceptions -Wno-multichar -Wno-error=unused-parameter -Wno-error=unused-but-set-variable
 $(combo_target)RELEASE_CFLAGS := -O3 -g -fstrict-aliasing -Wstrict-aliasing=3 -Werror=strict-aliasing -fno-tree-vectorize -fno-inline-functions -fno-unswitch-loops -fgcse-after-reload -fno-ipa-cp-clone -fno-vect-cost-model -Wno-error=unused-parameter -Wno-error=unused-but-set-variable
-$(combo_target)GLOBAL_LDFLAGS :=
+$(combo_target)GLOBAL_LDFLAGS := -Wl,-O1 -Wl,--as-needed -Wl,--relax -Wl,--sort-common -Wl,--gc-sections
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
 # Turn off strict-aliasing if we're building an AOSP variant without the
