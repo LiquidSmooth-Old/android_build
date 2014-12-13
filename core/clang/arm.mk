63c202f9ff44e3663c94470108e34d826428642f
Chet Kener <Cl3Kener@gmail.com>
2014-12-13 09:37:17 -0500

    armv7-a: More Specific Tunings per CPU
    * Also add some substitutions and ignores for clang.
    
    Change-Id: Ia6acd54835f63ec054f245d986f482fd3267434a

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -22,11 +22,19 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
   -fno-builtin-sin \
   -fno-strict-volatile-bitfields \
   -fno-align-jumps \
-  -Wa,--noexecstack
+  -Wa,--noexecstack \
+  -Wno-unused-local-typedefs \
+  -fpredictive-commoning \
+  -ftree-loop-distribute-patterns \
+  -fvect-cost-model \
+  -ftree-partial-pre \
+  -fipa-cp-clone \
+  -mvectorize-with-neon-quad
 
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
-  $(subst -mcpu=cortex-a15,-march=armv7-a,\
-  $(1))))
+  $(subst -mfpu=neon-vfpv3,-mfpu=neon,\
+  $(subst -mfpu=neon-vfpv4,-mfpu=neon,\
+  $(1)))))
 endef
