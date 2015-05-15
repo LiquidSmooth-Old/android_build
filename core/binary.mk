dfdadb838ccbeb5eb6742ca1af68ad9765ec5c33
Ethan <cannondalev2000@gmail.com>
2015-05-15 01:41:07 -0400

    Liquid Optimizations V1
    It occured to me that Instead of porting optimizations from optipop
    I should instead take what I like from there and what I like from other places
    figure out what works best and put that togeather. so here is the inital version
    of liquid optimizations. as of now it includes -O3 and Graphite both of them I used
    the implamentation from Optipop. Other things will be added soon.
    
    this commit also reverts the liquify flag
    
    Change-Id: Id85b328d3a1f06e4ebbc5ac0faeebd783d09d055

--- core/binary.mk
+++ core/binary.mk
@@ -97,9 +97,7 @@ else
   endif
 endif
 
-ifeq ($(LIQUIFY),$(filter $(LIQUIFY),TRUE true True))
-include $(BUILD_SYSTEM)/graphite.mk
-endif
+include $(BUILD_SYSTEM)/liquidsmooth.mk
 
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
---
+++ core/liquidsmooth.mk
@@ -0,0 +1,73 @@
+# GRAPHITE
+ifeq ($(GRAPHITE),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+LOCAL_DISABLE_GRAPHITE := \
+	libunwind \
+	libFFTEm \
+	libicui18n \
+	libskia \
+	libvpx \
+	libmedia_jni \
+	libstagefright_mp3dec \
+	libart \
+	libstagefright_amrwbenc \
+	libpdfium \
+	libpdfiumcore \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
+	libjni_filtershow_filters \
+	fio \
+	libwebrtc_spl \
+	libpcap \
+	libFraunhoferAAC
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fgraphite \
+	-fgraphite-identity \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
++else
+LOCAL_CONLYFLAGS := \
+	-fgraphite \
+	-fgraphite-identity \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fgraphite \
+	-fgraphite-identity \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+else
+LOCAL_CPPFLAGS := \
+	-fgraphite \
+	-fgraphite-identity \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+endif
+endif
+endif
+endif
+endif
