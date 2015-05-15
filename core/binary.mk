53d66f12fcf10dbf58c51731181d5112b62332e4
Ethan <cannondalev2000@gmail.com>
2015-05-15 01:41:07 -0400

    Liquid Optimizations V1
    It occured to me that Instead of porting optimizations from optipop
    I should instead take what I like from there and what I like from other places
    figure out what works best and put that togeather. so here is the inital version
    of liquid optimizations. as of now it includes -O3 and Graphite both of them I used
    the implamentation from Optipop. Other things will be added soon.
    
    this commit also reverts the liquify flag
    
    PS2: add strict aliasing
    PS3: make sure flags are being passed as they should be
    PS4: add LTO (link time optimizations)
    Change-Id: Id85b328d3a1f06e4ebbc5ac0faeebd783d09d055

--- core/binary.mk
+++ core/binary.mk
@@ -97,8 +97,89 @@ else
   endif
 endif
 
-ifeq ($(LIQUIFY),$(filter $(LIQUIFY),TRUE true True))
-include $(BUILD_SYSTEM)/graphite.mk
+
+# STRICT ALIASING   #
+ifeq ($(STRICT),true)
+ifneq ($(filter,$(LOCAL_MODULE)),)
+
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	$(STRICT_ALIASING_FLAGS)
+else
+LOCAL_CONLYFLAGS := \
+	$(STRICT_ALIASING_FLAGS)
+endif
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	$(STRICT_ALIASING_FLAGS)
+else
+LOCAL_CPPFLAGS := \
+	$(STRICT_ALIASING_FLAGS)
+endif
+ifndef LOCAL_CLANG
+LOCAL_CONLYFLAGS += \
+	$(STRICT_GCC_LEVEL)
+LOCAL_CPPFLAGS += \
+	$(STRICT_GCC_LEVEL)
+else
+LOCAL_CONLYFLAGS += \
+	$(STRICT_CLANG_LEVEL)
+LOCAL_CPPFLAGS += \
+	$(STRICT_CLANG_LEVEL)
+endif
+
+endif
+endif
+
+#       GRAPHITE       #
+ifeq ($(GRAPHITE),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+ifeq ($(filter $(DISABLE_GRAPHITE), $(LOCAL_MODULE)),)
+
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	$(GRAPHITE_FLAGS)
+else
+LOCAL_CONLYFLAGS := \
+	$(GRAPHITE_FLAGS)
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	$(GRAPHITE_FLAGS)
+else
+LOCAL_CPPFLAGS := \
+	$(GRAPHITE_FLAGS)
+endif
+
+endif
+endif
+endif
+endif
+
+ifeq ($(LTO),true)
+ifeq ($(filter, $(LOCAL_MODULE)),)
+
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+        $(LTO_FLAGS)
+else                                                                                                                                                                      
+LOCAL_CONLYFLAGS := \
+        $(LTO_FLAGS)
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+        $(LTO_FLAGS)
+else
+LOCAL_CPPFLAGS := \
+        $(LTO_FLAGS)
+endif
+
+endif
+endif
+endif
 endif
 
 # The following LOCAL_ variables will be modified in this file.
---
+++ core/liquidsmooth.mk
@@ -0,0 +1,48 @@
+# STRICT ALIASING
+STRICT_ALIASING_FLAGS := \
+    -fstrict-aliasing \
+    -Werror=strict-aliasing
+
+STRICT_GCC_LEVEL := \
+    -Wstrict-aliasing=3
+
+STRICT_CLANG_LEVEL := \
+    -Wstrict-aliasing=2
+
+#       GRAPHITE       #
+ DISABLE_GRAPHITE := \
+    libunwind \
+    libFFTEm \
+    libicui18n \
+    libskia \
+    libvpx \
+    libmedia_jni \
+    libstagefright_mp3dec \
+    libart \
+    libstagefright_amrwbenc \
+    libpdfium \
+    libpdfiumcore \
+    libwebviewchromium \
+    libwebviewchromium_loader \
+    libwebviewchromium_plat_support \
+    libjni_filtershow_filters \
+    fio \
+    libwebrtc_spl \
+    libpcap \
+    libsigchain \
+    libFraunhoferAAC
+
+GRAPHITE_FLAGS := \
+    -fgraphite \
+    -fgraphite-identity \
+    -floop-flatten \
+    -floop-parallelize-all \
+    -ftree-loop-linear \
+    -floop-interchange \
+    -floop-strip-mine \
+    -floop-block
+
+#        LTO        #
+LTO_FLAGS := \
+    -flto
+    -fuse-linker-plugin    
