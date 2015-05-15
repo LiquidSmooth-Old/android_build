9e2a90e3255f758ccc4d1b1491679b20189f174d
Ethan <cannondalev2000@gmail.com>
2015-05-14 23:21:59 -0400

    squashed revert liquify flag
    Revert "build: Update LIQUIFY flag for multiple case"
    
    This reverts commit 417d2e8d39130f550d7b680c3d0aa7400be3603d.
    
    Revert "LIQUIFY OPTIMIZATIONS V1"
    
    This reverts commit 6bd6ec527177dad3aff08f2cfe9e8c2e9695c952.
    
    Change-Id: I0578f6e82389c9a092d95ac6ea190637a07f59a5

--- core/binary.mk
+++ core/binary.mk
@@ -97,9 +97,7 @@ else
   endif
 endif
 
-ifeq ($(LIQUIFY),$(filter $(LIQUIFY),TRUE true True))
 include $(BUILD_SYSTEM)/graphite.mk
-endif
 
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
